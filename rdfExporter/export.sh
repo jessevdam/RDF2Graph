1316a1cb89c4d744497b23fc9cfb7b5138709cc2
Jesse c j van Dam <jesse.vandam@wur.nl>
2015-07-14 15:39:43 +0200

    The rdf exporter can now run independent from its directory

--- rdfExporter/export.sh
+++ rdfExporter/export.sh
@@ -20,9 +20,8 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cd -P $DIR
 mkdir -p ./temp
 
 #Get links from db
-tdbquery --loc $project --query queries/all.txt --results N3 > $outfile
+tdbquery --loc $project --query $DIR/queries/all.txt --results N3 > $outfile
 echo "export completed"
